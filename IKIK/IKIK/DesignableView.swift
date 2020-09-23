8376e086596a8c7a00881463dd90b504ba465633
서민주 <minju1307@gmail.com>
2020-09-23 15:55:36 +0900

    Add Login Page and Sign up Page

---
+++ IKIK/IKIK/DesignableView.swift
@@ -0,0 +1,36 @@
+//
+//  DesignableView.swift
+//  IKIK
+//
+//  Created by 서민주 on 2020/09/22.
+//  Copyright © 2020 minjuseo. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class DesignableView: UIView {
+    @IBInspectable var shadowColor: UIColor = UIColor.clear {
+        didSet {
+            layer.shadowColor = shadowColor.cgColor
+        }
+    }
+    
+    @IBInspectable var shadowRadius: CGFloat = 0 {
+        didSet {
+            layer.shadowRadius = shadowRadius
+        }
+    }
+    
+    @IBInspectable var shadowOpacity: CGFloat = 0 {
+        didSet {
+            layer.shadowOpacity = Float(shadowOpacity)
+        }
+    }
+    
+    @IBInspectable var shadowOffsetY: CGFloat = 0 {
+        didSet {
+            layer.shadowOffset.height = shadowOffsetY
+        }
+    }
+}
