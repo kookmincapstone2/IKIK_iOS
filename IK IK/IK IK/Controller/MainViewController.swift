6c12c8b90fa227c9dc24c1df7857ac864be9923a
서민주 <minju1307@gmail.com>
2020-11-03 17:34:19 +0900

    MainView load시 사용자가 가입된 방 목록 TableView로 불러오기

--- IK IK/IK IK/Controller/MainViewController.swift
+++ IK IK/IK IK/Controller/MainViewController.swift
@@ -10,44 +10,115 @@ import UIKit
 
 class MainViewController: UIViewController, UITableViewDataSource {
     
-    var user: User?
+    //    var user: User?
+    var myRoomList = [Room]()
+    var selectedRoom: Room?
+    let networkingService = NetworkingService()
     
     @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var mainTableView: UITableView!
     
+    
+    // MARK: - Functions
     override func viewDidLoad() {
         super.viewDidLoad()
+        NotificationCenter.default.addObserver(self, selector: #selector(self.refresh), name: NSNotification.Name(rawValue: "reloadRoomData"), object: nil)
         
-//        if let name = user?.name {
-//
-//            nameLabel.text = "Welcome, \(name.capitalized)"
-//        }
-        if let name = UserDefaults.standard.string(forKey: "username") {
+        if let name = UserDefaults.standard.string(forKey: "username"),
+            let userId = UserDefaults.standard.string(forKey: "userid") {
             
-            nameLabel.text = "Welcome, \(name.capitalized)"
+            nameLabel.text = "안녕하세요, \(name.capitalized)님"
+            getMyRooms(userId: Int(userId)!)
+        }
+        refresh()
+    }
+    
+    @objc func refresh() {
+        self.mainTableView.reloadData()
+    }
+    
+    @IBAction func getNewRoomData(_ sender: UIStoryboardSegue){
+        if let from = sender.source as? RoomCreateViewController {
+            print(from)
+            print(from.newRoom ?? "no room data")
+            myRoomList.append(from.newRoom!)
+            mainTableView.reloadData()
         }
     }
     
+    func getMyRooms(userId: Int) {
+        networkingService.getMyRooms(endpoint: "/room/member/management", userId: userId, completion: { [weak self] (result) in
+            
+            print(result)
+            switch result {
+                
+            case .success(let roomList):
+                self?.myRoomList = roomList.sorted(by: { $0.title < $1.title})
+                NotificationCenter.default.post(name: NSNotification.Name(rawValue: "reloadRoomData"), object: nil)
+                
+            case .failure(let error):
+                // did not enter any room yet
+                self?.myRoomList = []
+                print("getting room error", error)
+                break
+            }
+        })
+    }
+    
+    // MARK: - tableView
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return myRoomList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! LectureCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "LectureCell", for: indexPath) as! LectureCell
         
-        cell.titleLabel.text = "수업명"
-        cell.timeLabel.text = "요일 00:00~24:59"
-        cell.detailLabel.text = "OOO교수님 7호관 424호"
+        let roomTemp = myRoomList[indexPath.row]
+        
+        cell.titleLabel.text = roomTemp.title
+        cell.timeLabel.text = "최대 정원: "+String(roomTemp.maximumPopulation)
+        cell.detailLabel.text = "초대 코드: " + (roomTemp.inviteCode ?? "")
+
         return cell
     }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
+        selectedRoom = myRoomList[indexPath.row]
+        tableView.deselectRow(at: indexPath, animated: true)
+        if let rank = UserDefaults.standard.string(forKey: "rank") {
+            switch rank {
+            case "teacher":
+                print("perform teacher")
+                performSegue(withIdentifier: "teacherView", sender: tableView)
+                
+            case "student":
+                print("perform student")
+                performSegue(withIdentifier: "studentView", sender: tableView)
+                
+            default:
+                print("rank error")
+            }
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        print("prepare")
+        if segue.identifier == "teacherView" {
+            let destination = segue.destination as! RecordViewController
+            destination.roomData = selectedRoom
+            
+        } else if segue.identifier == "studentView" {
+            let destination = segue.destination as! RecordViewController
+            destination.roomData = selectedRoom
+        }
+    }
+    
+    
+//     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+//        if editingStyle == .delete {
+//            myRoomList.remove(at: indexPath.row)
+//            tableView.deleteRows(at: [indexPath], with: .fade)
+//        }
+//    }
 }
-
-
-//// Parse JSON data
-//       if let data = data {
-//           self.loans = self.parseJsonData(data: data)
-//
-//           // Reload table view
-//           OperationQueue.main.addOperation({
-//               self.tableView.reloadData()
-//           })
-//       }
