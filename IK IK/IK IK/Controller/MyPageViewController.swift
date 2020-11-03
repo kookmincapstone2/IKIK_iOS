8a28a3ce2720c2bb128cbd7ee71689a1c6766f26
서민주 <minju1307@gmail.com>
2020-11-03 17:32:22 +0900

    로그아웃시 자동로그인 되지않도록 UserDefaults에서 정보 제거

--- IK IK/IK IK/Controller/MyPageViewController.swift
+++ IK IK/IK IK/Controller/MyPageViewController.swift
@@ -9,29 +9,30 @@
 import UIKit
 
 class MyPageViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
+    
     @IBAction func logoutTapped(_ sender: UIButton) {
-            // after user has successfully logged out
-      
+        
+        UserDefaults.standard.removeObject(forKey: "email")
+        UserDefaults.standard.removeObject(forKey: "pw")
+        UserDefaults.standard.removeObject(forKey: "username")
+        UserDefaults.standard.removeObject(forKey: "userid")
+        
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         let loginNavController = storyboard.instantiateViewController(identifier: "LoginNavigationController")
-
+        
         (UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate)?.changeRootViewController(loginNavController)
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
+//    // MARK: - tableView
+//    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+//        return 0
+//    }
+//
+//    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return 0
+//    }
 }
