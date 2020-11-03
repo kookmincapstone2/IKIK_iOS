6a649de64f53658f80bbbe988180dd811fc8d4d5
서민주 <minju1307@gmail.com>
2020-11-03 17:28:59 +0900

    Add Custom TableViewCell

--- IK IK/IK IK/Controller/AttendanceCell.swift
+++ IK IK/IK IK/Controller/AttendanceCell.swift
@@ -1,27 +1,25 @@
 //
-//  LectureCell.swift
+//  AttendanceCell.swift
 //  IK IK
 //
-//  Created by 서민주 on 2020/10/07.
+//  Created by 서민주 on 2020/10/22.
 //  Copyright © 2020 minjuseo. All rights reserved.
 //
 
 import UIKit
 
-class LectureCell: UITableViewCell {
-
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var timeLabel: UILabel!
-    @IBOutlet weak var detailLabel: UILabel!
+class AttendanceCell: UITableViewCell {
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var attendanceImageView: UIImageView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
-
 }
