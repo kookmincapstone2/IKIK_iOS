e9bc2cd736ef4edacad55ec53d4445a7dbb54391
서민주 <minju1307@gmail.com>
2020-11-16 01:25:22 +0900

    RoomEditView 랭크에 따라 달라지도록 변경

--- IK IK/IK IK/Controller/RoomEditViewController.swift
+++ IK IK/IK IK/Controller/RoomEditViewController.swift
@@ -10,15 +10,30 @@ import UIKit
 
 class RoomEditViewController: UIViewController {
 
+    var roomData: Room?
+    
     @IBOutlet weak var titleTextField: FormTextField!
     @IBOutlet weak var populationTextField: FormTextField!
+    @IBOutlet weak var deleteButton: UIButton!
+    @IBOutlet weak var leaveButton: UIButton!
+    
     let networkingService = NetworkingService()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        titleTextField.text =
-//        populationTextField.text =
+        titleTextField.text = roomData?.title
+        populationTextField.text = (roomData?.maximumPopulation).map{ String($0) }
+        
+        if let rank = UserDefaults.standard.string(forKey: "rank") {
+            if rank == "student" {
+                titleTextField.isUserInteractionEnabled = false
+                populationTextField.isUserInteractionEnabled = false
+                deleteButton.isHidden = true
+            } else {
+                leaveButton.isHidden = true
+            }
+        }
     }
     
     // MARK: - IBActions
@@ -27,14 +42,31 @@ class RoomEditViewController: UIViewController {
         guard
             let userId = UserDefaults.standard.string(forKey: "userid"),
             let title = titleTextField.text,
-            let population = populationTextField.text
+            let population = populationTextField.text,
+            let roomId = roomData?.roomId
             else { return }
         
-        roomEditRequest(userId: userId, title: title, population: population)
+        roomEditRequest(userId: userId, roomId: String(roomId), title: title, population: population)
     }
     
     @IBAction func didTapDeleteButton(_ sender: Any) {
         
+        guard
+            let userId = UserDefaults.standard.string(forKey: "userid"),
+            let roomId = roomData?.roomId
+            else { return }
+        
+        roomDeleteRequest(userId: userId, roomId: String(roomId))
+    }
+    
+    @IBAction func didTapLeaveButton(_ sender: Any) {
+        
+        guard
+            let userId = UserDefaults.standard.string(forKey: "userid"),
+            let roomId = roomData?.roomId
+            else { return }
+        
+        roomLeaveRequest(userId: userId, roomId: String(roomId))
     }
     
     @IBAction func didTapCancelButton(_ sender: Any) {
@@ -47,18 +79,16 @@ class RoomEditViewController: UIViewController {
         self.view.endEditing(true)
     }
     
-    func roomEditRequest(userId: String, title: String, population: String) {
-        let parameters = ["user_id": userId, "title": title, "maximum_population": population]
+    func roomEditRequest(userId: String, roomId: String, title: String, population: String) {
         
-        networkingService.handleRoom(method: "PUT", endpoint: "/room/management", parameters: parameters, completion: { [weak self] (result) in
+        let parameters = ["user_id": userId, "room_id": roomId, "title": title, "maximum_population": population]
+
+        networkingService.request(endpoint: "/room/management", method: "PUT", parameters: parameters, completion: { [weak self] (result) in
             
             print(result)
             switch result {
-                
             case .success(let room):
-                //                invitation code sharing modal, using clipboard
-                //                UIPasteboard.general.string
-                NotificationCenter.default.post(name: NSNotification.Name(rawValue: "reloadRoomData"), object: nil)
+                NotificationCenter.default.post(name: NSNotification.Name(rawValue: "reloadRoomData"), object: nil, userInfo: ["room": room!])
                 
             case .failure(let error):
                 // did not enter any room yet
@@ -69,4 +99,45 @@ class RoomEditViewController: UIViewController {
         })
     }
     
+    func roomDeleteRequest(userId: String, roomId: String) {
+        
+        let parameters = ["user_id": userId, "room_id": roomId]
+        
+        networkingService.request(endpoint: "/room/management", method: "DELETE", parameters: parameters, completion: { [weak self] (result) in
+            
+            print(result)
+            switch result {
+            case .success:
+                // TODO: delete alert, pop segue
+                NotificationCenter.default.post(name: NSNotification.Name(rawValue: "reloadRoomData"), object: nil)
+                self?.performSegue(withIdentifier: "unwindToMainVC", sender: self)
+                
+            case .failure(let error):
+                print("deleting room error", error)
+                break
+            }
+            self?.dismiss(animated: true, completion: nil)
+        })
+    }
+    
+    func roomLeaveRequest(userId: String, roomId: String) {
+        
+        let parameters = ["user_id": userId, "room_id": roomId]
+        
+        networkingService.request(endpoint: "/room/member/management", method: "DELETE", parameters: parameters, completion: { [weak self] (result) in
+            
+            print(result)
+            switch result {
+            case .success:
+                // TODO: delete alert, pop segue
+                NotificationCenter.default.post(name: NSNotification.Name(rawValue: "reloadRoomData"), object: nil)
+                self?.performSegue(withIdentifier: "unwindToMainVC", sender: self)
+                
+            case .failure(let error):
+                print("deleting room error", error)
+                break
+            }
+            self?.dismiss(animated: true, completion: nil)
+        })
+    }
 }
