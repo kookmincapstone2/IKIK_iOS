8dec80546c310eee86d8bad04582244bee44a03e
서민주 <minju1307@gmail.com>
2020-11-25 01:29:43 +0900

    Add View of Checking Attendance Status

---
+++ IK IK/IK IK/Controller/Check/CheckCell.swift
@@ -0,0 +1,26 @@
+//
+//  CheckCell.swift
+//  IK IK
+//
+//  Created by 서민주 on 2020/11/22.
+//  Copyright © 2020 minjuseo. All rights reserved.
+//
+
+import UIKit
+
+class CheckCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
