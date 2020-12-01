087cf54b9a17d540a124b39ad5c2bd0d98a0e47d
서민주 <minju1307@gmail.com>
2020-12-02 00:32:09 +0900

    LectureCell xib이용해 생성

--- IK IK/IK IK/Controller/Check/LectureCell.swift
+++ IK IK/IK IK/Controller/Check/LectureCell.swift
@@ -2,12 +2,20 @@
 //  LectureCell.swift
 //  IK IK
 //
-//  Created by 서민주 on 2020/11/24.
+//  Created by 서민주 on 2020/12/01.
 //  Copyright © 2020 minjuseo. All rights reserved.
 //
 
 import UIKit
 
 class LectureCell: UICollectionViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var populationLabel: UILabel!
     
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
 }
